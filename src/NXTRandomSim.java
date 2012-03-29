64d4f295eeb86572cd1b86108aaf1d6c6de04143
Carlos Barboza <theman@ekosdeux.com>
2012-03-29 11:52:25 -0500

    Adjusting obstacle position through use of unit circle.

--- src/NXTRandomSim.java
+++ src/NXTRandomSim.java
@@ -3,6 +3,7 @@
 
 import ch.aplu.nxtsim.*;
 import ch.aplu.jgamegrid.*;
+
 import java.awt.*;
 import java.util.*;
 
@@ -16,11 +17,10 @@ import java.io.IOException;
 import javax.imageio.ImageIO;
 */
 
-public class NXTRandomSim
-{
+public class NXTRandomSim {
   static Random generator = new Random(System.currentTimeMillis());
-  static Random generatorX = new Random(3*System.currentTimeMillis());
-  static Random generatorY = new Random(7*System.currentTimeMillis());
+  static Random generatorX = new Random(3 * System.currentTimeMillis());
+  static Random generatorY = new Random(7 * System.currentTimeMillis());
   NxtRobot robot = new NxtRobot();
   Gear gear = new Gear();
   LightSensor ls1 = new LightSensor(SensorPort.S1);
@@ -35,8 +35,7 @@ public class NXTRandomSim
   int randomNumber = generator.nextInt(500);
   int reset = 0;
 
-  public NXTRandomSim()
-  {
+  public NXTRandomSim() {
     robot.addPart(gear);
     robot.addPart(ls1);
     robot.addPart(ls2);
@@ -44,30 +43,32 @@ public class NXTRandomSim
     gear.forward();
     gear.setSpeed(110);
 
-    while (true)
-    {
+    while (true) {
       currentPosition.setLocation(gear.getX(), gear.getY());
-      if (ts1.isPressed())
-      {
-        if (!obstacleList.contains(currentPosition))
-        {
-          obstacleList.add(new Point((int)(currentPosition.getX()+ 15*Math.cos(gear.getDirection())),(int)(currentPosition.getY()+ 15*Math.sin(gear.getDirection()))));
-          System.out.println("Obstacle "+obstacleList.size());
+      if (ts1.isPressed()) {
+        if (!obstacleList.contains(currentPosition)) {
+          if (gear.getDirection() >= 0 && gear.getDirection() < 90)
+            obstacleList.add(new Point((int) (currentPosition.getX() + 15 * Math.cos(gear.getDirection())), (int) (currentPosition.getY() + 15 * Math.sin(gear.getDirection()))));
+          if (gear.getDirection() >= 90 && gear.getDirection() < 180)
+            obstacleList.add(new Point((int) (currentPosition.getX() + 15 * Math.cos(gear.getDirection())), (int) (currentPosition.getY() + 15 * Math.sin(gear.getDirection()))));
+          if (gear.getDirection() >= 180 && gear.getDirection() < 270)
+            obstacleList.add(new Point((int) (currentPosition.getX() + 15 * Math.cos(gear.getDirection())), (int) (currentPosition.getY() + 15 * Math.sin(gear.getDirection()))));
+          else
+            obstacleList.add(new Point((int) (currentPosition.getX() + 15 * Math.cos(gear.getDirection())), (int) (currentPosition.getY() + 15 * Math.sin(gear.getDirection()))));
+          System.out.println("Obstacle " + obstacleList.size());
           gear.backward(600);
           //gear.setDirection(randomNumber - gear.getDirection());
           gear.right(200);
           gear.forward();
-          System.out.println("("+obstacleList.getLast().getX()+", "+ obstacleList.getLast().getY()+")");
+          System.out.println("(" + obstacleList.getLast().getX() + ", " + obstacleList.getLast().getY() + ")");
         }
         checkLineColor();
       }
-      synchronized(obstacleList)
-      {
+      synchronized (obstacleList) {
         ListIterator<Point> obstacleListChecker = obstacleList.listIterator();
-        while (obstacleListChecker.hasNext())
-        {
+        while (obstacleListChecker.hasNext()) {
           obstaclePosition = obstacleListChecker.next();
-          if (currentPosition.distance(obstaclePosition) < 40){
+          if (currentPosition.distance(obstaclePosition) < 40) {
             gear.backward(600);
             if (generator.nextInt(500) < 250)
               gear.left(200);
@@ -75,26 +76,24 @@ public class NXTRandomSim
               gear.right(200);
             //gear.setDirection(randomNumber - gear.getDirection());
             gear.forward();
-            System.out.println("Distance "+currentPosition.distance(obstaclePosition));
-            System.out.println("Headed "+gear.getDirection());
+            System.out.println("Distance " + currentPosition.distance(obstaclePosition));
+            System.out.println("Headed " + gear.getDirection());
           }
         }
       }
-      if (gear.getX() < 0 || gear.getX() > 500 || gear.getY() < 0 || gear.getY() >500)
-      {
-        gear.backward(1500);
+      if (gear.getX() < 0 || gear.getX() > 500 || gear.getY() < 0 || gear.getY() > 500) {
+        //gear.backward(1500);
         //gear.setDirection(randomNumber - gear.getDirection());
-        gear.forward();
+        //gear.forward();
         robot.reset();
         reset++;
-        System.out.println("Reset "+reset);
+        System.out.println("Reset " + reset);
       }
       checkLineColor();
     }
   }
 
-  public void checkLineColor()
-  {
+  public void checkLineColor() {
     int v = 500;
     double r = 0.;
     int v1 = ls1.getValue();
@@ -113,62 +112,60 @@ public class NXTRandomSim
   }
 
   @SuppressWarnings("unused")
-  private static void _init(GameGrid gg)
-  {
+  private static void _init(GameGrid gg) {
     GGBackground bg = gg.getBg();
     bg.setPaintColor(Color.black);
-    bg.fillRectangle(new Point(9,9), new Point(491,491));
+    bg.fillRectangle(new Point(9, 9), new Point(491, 491));
   }
 
-  public static void main(String[] args)
-  {
+  public static void main(String[] args) {
     new NXTRandomSim();
   }
-/*
-    public static void obstacleFactory() 
-    {
-        int width = 30;
-        int height = 30;
 
-        // Create a buffered image in which to draw
-        //BufferedImage bufferedImage = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+  /*
+      public static void obstacleFactory()
+      {
+          int width = 30;
+          int height = 30;
 
-        GraphicsEnvironment environment = 
-                GraphicsEnvironment.getLocalGraphicsEnvironment();
+          // Create a buffered image in which to draw
+          //BufferedImage bufferedImage = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
 
-        GraphicsDevice device = 
-                environment.getDefaultScreenDevice();
+          GraphicsEnvironment environment =
+                  GraphicsEnvironment.getLocalGraphicsEnvironment();
 
-        GraphicsConfiguration config = device.getDefaultConfiguration();
+          GraphicsDevice device =
+                  environment.getDefaultScreenDevice();
 
-        // Create an image that supports transparent pixels
-        BufferedImage bufferedImage = config.createCompatibleImage(width, height, 
-                Transparency.TRANSLUCENT);
+          GraphicsConfiguration config = device.getDefaultConfiguration();
 
-        // Create a graphics contents on the buffered image
-        Graphics2D g2d = bufferedImage.createGraphics();
+          // Create an image that supports transparent pixels
+          BufferedImage bufferedImage = config.createCompatibleImage(width, height,
+                  Transparency.TRANSLUCENT);
 
-        // Draw graphics
-        g2d.setColor(Color.yellow);
-        g2d.fillOval(0, 0, width, height);
+          // Create a graphics contents on the buffered image
+          Graphics2D g2d = bufferedImage.createGraphics();
 
-        // Graphics context no longer needed so dispose it
-        g2d.dispose();
+          // Draw graphics
+          g2d.setColor(Color.yellow);
+          g2d.fillOval(0, 0, width, height);
+
+          // Graphics context no longer needed so dispose it
+          g2d.dispose();
 
 
-        RenderedImage rendImage = bufferedImage;
-        //return bufferedImage; 
-        // Write generated image to a file
-        try {
-            // Save as PNG
-            File file = new File("src/sprites/Circle.jpg");
-            ImageIO.write(rendImage, "jpg", file);
-        } catch (IOException e) {}
-    }
-*/
+          RenderedImage rendImage = bufferedImage;
+          //return bufferedImage;
+          // Write generated image to a file
+          try {
+              // Save as PNG
+              File file = new File("src/sprites/Circle.jpg");
+              ImageIO.write(rendImage, "jpg", file);
+          } catch (IOException e) {}
+      }
+  */
   // ------------------ Environment --------------------------
-  static
-  {        // TODO get obstacle generation to work
+  static {        // TODO get obstacle generation to work
     //RenderedImage rendImage = obstacleFactory();
 /*
         obstacleFactory();
@@ -179,10 +176,10 @@ public class NXTRandomSim
             e.printStackTrace();
         }
 */
-    NxtContext.setStartPosition(250,250);
+    NxtContext.setStartPosition(250, 250);
     NxtContext.setStartDirection(90);
     //NxtContext.useObstacle("sprites/Circle.jpg", 250, 475);
-    for(int i = 0; i < 10; i++) {
+    for (int i = 0; i < 10; i++) {
       randomXCoordinate = generatorX.nextInt(475) + 10;
       randomYCoordinate = generatorY.nextInt(475) + 10;
       if (randomXCoordinate > 25 && randomXCoordinate < 475 && randomYCoordinate > 25 && randomYCoordinate < 475)
